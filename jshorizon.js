02d5c57afbc6555d2880346e6ed044ff022a8c33
it4e <limpands@gmail.com>
2016-01-14 19:45:38 +0100

    UPDATE 1
    - calc_body_width changed to calc_width
    - now using percentage instead of pixels for responsiveness; dividing by viewport width
    - added support for element exceptions: add_exception method

--- jshorizon.js
+++ jshorizon.js
@@ -1,15 +1,43 @@
+/*
+ *
+ *  JSHorizon, https://www.github.org/it4e/jshorizon
+ *  it4e - 2016
+ *
+ */
+
+
+// Horizontal object
 var Horizon = function(container) {
     this.container = container;
-    this.body_width = 0;
+    this.container_width = 0;
+    this.exceptions = new Array();
 }
 
+// Add element as an exception so its width will not be included
+Horizon.prototype.add_exception = function(exception) {
+    this.exceptions[this.exceptions.length] = exception;
+}
 
-Horizon.prototype.calc_body_width = function() {
+// Calculate and set container width
+Horizon.prototype.calc_width = function() {
     var children = this.container.children;
     var skip;
     
-    for(var i = 0; i < children.length; i++)
-        this.body_width += children[i].offsetWidth;
+    // Loop through elements and handle exceptions
+    for(var i = 0; i < children.length; i++) {
+        for(var x = 0; x < this.exceptions.length; x++)
+            if(children[i] == this.exceptions[x]) {
+                skip = true;
+                break;
+            }
 
-    document.getElementsByTagName("BODY")[0].style.width = this.body_width + "px";
+        if(skip) {
+            skip = false;
+            continue;
+        }
+
+        this.container_width += children[i].offsetWidth;
+    }
+
+    this.container.style.width = (this.container_width / document.documentElement.clientWidth) * 100 + "%";
 }
